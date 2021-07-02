24728586341603364e6cd5c3b71b128584887265
Igluminati <31823943+Igluminati@users.noreply.github.com>
2021-07-02 12:25:58 +0100

    Create __init__.py

---
+++ interface/__init__.py
@@ -0,0 +1,12 @@
+"""
+This module contains methods for interacting with the user interface, ie.
+printing to the user and getting input from the user through the shell.
+"""
+
+from .built_in_methods import *
+from .generate_readable_list import *
+from .get_input import *
+from .get_boolean_input import *
+from .get_integer_input import *
+from .get_command import *
+from .print_multiple_lines import *
